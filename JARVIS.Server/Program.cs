6d825ff1d774943b2bdc0736c9e3ea9246ac4ee0
Matthew Davey <reapazor@gmail.com>
2017-10-20 08:45:41 -0400

    Starting to prep for database

--- JARVIS.Server/Program.cs
+++ JARVIS.Server/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Text;
 using System.Threading;
 using Grapevine.Server;
 
@@ -7,10 +6,30 @@ namespace JARVIS.Server
 {
     class Program
     {
+        public static string Port = "8080";
+        public static string Database = "JARVIS.db";
+        public static bool HasCustomDatabase = false;
+        public static bool HasCustomPort = false;
+
         static ManualResetEvent _quitEvent = new ManualResetEvent(false);
+
 
         public static void Main(string[] args)
         {
+			// Handle Arguments
+			switch ( args.Length ) {
+                case 2:
+					Program.Port = args[0];
+                    Program.HasCustomPort = true;
+					Program.Database = args[1];
+                    Program.HasCustomDatabase = true;
+                    break;
+                case 1:
+                    Program.Port = args[0];
+                    Program.HasCustomPort = true;
+                    break;
+            }
+          
 			Console.CancelKeyPress += (sender, eArgs) => {
 				_quitEvent.Set();
 				eArgs.Cancel = true;
@@ -18,7 +37,7 @@ namespace JARVIS.Server
 
 			using (var server = new RestServer())
 			{
-                server.Port = "8080";
+                server.Port = Program.Port;
 				server.LogToConsole().Start();
 				_quitEvent.WaitOne();
 				server.Stop();
