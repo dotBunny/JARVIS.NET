6b5e4a91559d99bde1467e251feab5527e021dd0
Matthew Davey <matthew.davey@dotbunny.com>
2017-10-20 22:42:51 -0400

    Starting Counter Support
    Counters will reside in the database, with endpoints on the REST API, with socket pushes updating values to files stored on the shard machines.

---
+++ JARVIS.Server/Services/Web/Counter.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using Grapevine.Interfaces.Server;
+using Grapevine.Server;
+using Grapevine.Server.Attributes;
+using Grapevine.Shared;
+
+namespace JARVIS.Server.Services.Web
+{
+    [RestResource]
+    public class Counter
+    {
+        [RestRoute(HttpMethod = HttpMethod.GET, PathInfo = "/counter/plus")]
+        [RestRoute(HttpMethod = HttpMethod.GET, PathInfo = "/counter/plus/")]
+        public IHttpContext Plus(IHttpContext context)
+        {
+
+
+            //Shared.Log.Message("DB", "Setting Server.Host: " + optionHost.Value());
+            //DB.Connection.InsertOrReplace(new Tables.Settings()
+            //{
+            //    Name = "Server.Host",
+            //    Value = optionHost.Value()
+            //});
+
+
+            //string[] parameters = Shared.Web.GetParameters(context.Request.RawUrl, context.Request.PathInfo);
+
+            //// Build argument list
+            //string args = "";
+            //foreach (string s in parameters)
+            //{
+            //    args += s.Trim() + " ";
+            //}
+            //args.Trim();
+
+            //Program.Socket.SendToAllSessions("Wirecast.Layers", args);
+
+            //context.Response.SendResponse("OK");
+            return context;
+        }
+
+        [RestRoute(HttpMethod = HttpMethod.GET, PathInfo = "/counter/minus")]
+        [RestRoute(HttpMethod = HttpMethod.GET, PathInfo = "/counter/minus/")]
+        public IHttpContext Minus(IHttpContext context)
+        {
+            return context;
+        }
+
+        [RestRoute(HttpMethod = HttpMethod.GET, PathInfo = "/counter/set")]
+        [RestRoute(HttpMethod = HttpMethod.GET, PathInfo = "/counter/set/")]
+        public IHttpContext Set(IHttpContext context)
+        {
+            return context;
+        }
+    }
+}
---
+++ JARVIS.Server/Tables/Counters.cs
@@ -0,0 +1,16 @@
+﻿using SQLite.Net.Attributes;
+
+namespace JARVIS.Server.Tables
+{
+    public class Counters : ITable
+    {
+        public static string GetTableName()
+        {
+            return "Counters";
+        }
+        [PrimaryKey, MaxLength(128), NotNull]
+        public string Name { get; set; }
+        [MaxLength(128)]
+        public int Value { get; set; }
+    }
+}
