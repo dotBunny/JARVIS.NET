dac9133c113221325fb1d69fccbb5af6d98f3a1b
Matthew Davey <matthew.davey@dotbunny.com>
2017-10-22 18:43:52 -0400

    Stub for Sequencer
    This will take multiple commands and sequencially execute them.

---
+++ JARVIS.Core/Services/Web/Endpoints/Sequence.cs
@@ -0,0 +1,23 @@
+﻿using Grapevine.Interfaces.Server;
+using Grapevine.Server;
+using Grapevine.Server.Attributes;
+using Grapevine.Shared;
+
+namespace JARVIS.Core.Services.Web.Endpoints
+{
+    [RestResource]
+    public class Sequence
+    {
+        [RestRoute(HttpMethod = HttpMethod.GET, PathInfo = "/sequence")]
+        [RestRoute(HttpMethod = HttpMethod.GET, PathInfo = "/sequence/")]
+        public IHttpContext Command(IHttpContext context)
+        {
+
+            // Special where we look at 
+            // ?command1=wirecast.layer&args=&command2=counter.plus&args2=crashes
+
+            context.Response.SendResponse(Shared.Net.WebSuccessCode);
+            return context;
+        }
+    }
+}
